02f4c3f8b93a7dfd0c2153cfe8603f9c3dbdf6aa
Jack <luoxiaoyu@emodor.com>
2020-06-16 17:22:16 +0800

    initial commit

--- index.js
+++ index.js
@@ -1,52 +1,70 @@
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs-extra');
 const process = require('process');
-// const inquirer = require('inquirer');
+const inquirer = require('inquirer');
 
 const crtPath = process.cwd();
-const projectConfigPath = path.resolve(crtPath, '../project.config.json');
-const projectConfigJson = getJson(projectConfigPath);
-const { miniprogramRoot, condition } = projectConfigJson;
-const appPath = path.resolve(crtPath, '..', miniprogramRoot, './app.json');
-const appJson = getJson(appPath);
-const configPath = path.resolve(crtPath, './config.json');
-const configJson = getJson(configPath);
-const distPath = path.resolve(crtPath, './dist');
+const configPath = path.resolve(__dirname, './config.json');
+const configJson = fs.readJsonSync(configPath);
+const projectConfigPath = path.resolve(crtPath, './project.config.json');
+const projectConfigJson = fs.readJsonSync(projectConfigPath);
+const { miniprogramRoot } = projectConfigJson;
+const appPath = path.resolve(crtPath, configJson.switch === 'on' ? configJson.miniprogramRoot : miniprogramRoot, './app.json');
+const appJson = fs.readJsonSync(appPath);
+const distPath = path.resolve(__dirname, './dist');
+const miniprogramRootDev = 'client_dev/';
 
-const { pages, subpackages, preloadRule } = appJson;
-const conditionPages = condition.miniprogram.list;
+const { pages, subpackages } = appJson;
 
-// inquirer
-//   .prompt([
-//     /* Pass your questions in here */
-//   ])
-//   .then(answers => {
-//     // Use user feedback for... whatever!!
-//   })
-//   .catch(error => {
-//     if(error.isTtyError) {
-//       // Prompt couldn't be rendered in the current environment
-//     } else {
-//       // Something else when wrong
-//     }
-//   });
-
-start();
-
-function start() {
-  saveFilesToDist();
-}
-
-function getJson(filePath) {
-  const str = fs.readFileSync(filePath, { encoding: 'utf8' });
-  return JSON.parse(str);
-}
-
-function saveFilesToDist() {
-  const inited = configJson.switch === 'on';
-  if (!inited) {
-    fs.copyFileSync(projectConfigPath, path.resolve(distPath, './project.config.json'));
-    fs.copyFileSync(appPath, path.resolve(distPath, './app.json'));
-    // fs.writeFileSync(configPath, JSON.stringify({ ...configJson, switch: 'on' }, null, '  '))
-  }
-}
+inquirer
+  .prompt([
+    {
+      type: "checkbox",
+      name: "packages",
+      choices: subpackages.map(p => p.root),
+    },
+  ])
+  .then((answers) => {
+    const { packages } = answers;
+    if (packages && packages.length) {
+      const packagesMap = packages.reduce((pV, cV) => {
+        pV[cV] = true;
+        return pV;
+      }, {});
+      const excludes = subpackages.filter(pk => !packagesMap[pk.root]);
+      const distClientPath = path.resolve(projectConfigPath, '../', miniprogramRootDev);
+      fs.copySync(path.resolve(projectConfigPath, '../', configJson.switch === 'on' ? configJson.miniprogramRoot : miniprogramRoot), distClientPath);
+      excludes.forEach(pk => {
+        fs.remove(path.resolve(distClientPath, pk.root), err => {
+          if (err) {
+            console.error(err);
+          }
+        });
+      });
+      const pks = subpackages.filter(pk => packagesMap[pk.root]);
+      fs.writeJSONSync(path.resolve(distClientPath, './app.json'), {
+        ...appJson,
+        pages: Array.prototype.concat.apply(pages, pks.map(pk => pk.pages.map(p => `${pk.root}/${p}`))),
+        subpackages: [],
+        preloadRule: {},
+      }, {
+        spaces: 2,
+      });
+      fs.writeJSONSync(projectConfigPath, {
+        ...projectConfigJson,
+        miniprogramRoot: miniprogramRootDev,
+      }, {
+        spaces: 2,
+      });
+      if (configJson.switch !== 'on') {
+        fs.writeJSONSync(configPath, {
+          ...configJson,
+          switch: 'on',
+          miniprogramRoot,
+        }, {
+          spaces: 2,
+        });
+      }
+    }
+  })
+  .catch(console.error);
